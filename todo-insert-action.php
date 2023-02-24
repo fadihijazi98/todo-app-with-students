adb123d91a1c0d280d9f6517aa98d3c5d7e17c8f
SafaFahmawi <fahmawisafa@gmail.com>
2023-02-24 13:25:22 +0200

    save modified to move between pages

--- todo-insert-action.php
+++ todo-insert-action.php
@@ -1,12 +1,45 @@
 <?php 
 
 session_start();
-$_SESSION["items"][]=[
+
+$generated_id=generateId();
+
+$_SESSION['items']['todo'][$generated_id]=[
 "title"=>$_POST["title"],
-"discription"=>$_POST["discription"],
-"status"=>"todo",
+"description"=>$_POST["description"],
 "created_at"=>date("Y-m-d H:i:s")
 ];
 
 header("Location:todo.php");
+
+function generateId(){
+
+    if(! key_exists('items' ,$_SESSION)){
+        return 1;
+    }
+    
+        $items=$_SESSION['items'];
+
+        $todoItems=[0];
+        $completedItems=[0];
+        $deletedItems=[0];
+
+        if(key_exists('todo' ,$items)){
+            $todoItems=array_keys($items['todo']);
+        }
+        
+        if(key_exists('completed' ,$items)){
+            $completedItems=array_keys($items['completed']);
+        }
+        
+        if(key_exists('deleted' ,$items)){
+            $deletedItems=array_keys($items['deleted']);
+        }
+        
+        $maxIdOfTodo=max($todoItems);
+        $maxIdOfComplete=max($completedItems);
+        $maxIdOfDelete=max($deletedItems);
+
+        return max($maxIdOfTodo, $maxIdOfComplete, $maxIdOfDelete) + 1;
+}
 ?>
