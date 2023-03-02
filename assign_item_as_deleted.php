129b045147b277437afdeff1c6f02975caeb1bde
zeina adas <z.b.adas@students.ptuk.edu.ps>
2023-03-03 01:54:17 +0200

    not clean code

--- assign_item_as_deleted.php
+++ assign_item_as_deleted.php
@@ -1,1 +1,30 @@
 <?php
+
+session_start();
+
+$id = $_POST['item_id'];
+$deleted_from = $_POST['delete_from'];
+
+if ($deleted_from == "completed_item") {
+    $item = $_SESSION['items']['completed'][$id];
+    $completed_at = $item['completed_at'];
+    unset($_SESSION['items']['completed'][$id]);
+
+} else {
+    $item = $_SESSION['items']['todo'][$id];
+    $completed_at = null;
+    unset($_SESSION['items']['todo'][$id]);
+}
+
+$_SESSION['items']['deleted'][$id] = [
+    'title' => $item['title'],
+    'description' => $item['description'],
+    'created_at' => $item['created_at'],
+    'completed_at' => $completed_at,
+    'deleted_at' => date("Y-m-d H:i:s"),
+    'deleted_from' => $deleted_from
+];
+
+$_SESSION['message'] = "the '" . $item['title'] . "' is deleted now.";
+header("Location:deleted.php");
+?>
--- assign_todo_item_as_completed.php
+++ assign_todo_item_as_completed.php
@@ -1,1 +1,23 @@
 <?php
+
+session_start();
+
+$id = $_POST["item_id"];
+
+$todoItem = $_SESSION["items"]["todo"][$id];
+
+$_SESSION["items"]["completed"][$id]=[
+    "title"=>$todoItem["title"],
+    "description"=>$todoItem["description"],
+    "created_at"=>$todoItem["created_at"],
+    "completed_at"=>date("Y-m-d H:i:s")
+];
+
+$_SESSION["message"] = "The '{$todoItem["title"]}' is completed now :)";
+
+unset($_SESSION["items"]["todo"][$id]);
+
+header("Location:completed.php");
+
+
+?>
