a1857da7ed8ee51b977861a8f64a7e9db746bc5f
SafaFahmawi <fahmawisafa@gmail.com>
2023-03-03 10:39:07 +0200

    clean-code(01)

---
+++ actions/assign-todo-item-as-deleted.php
@@ -0,0 +1,30 @@
+<?php 
+
+session_start();
+
+$id= $_POST['item_id'];
+$deleted_from = $_POST['delete_from'];
+
+if($deleted_from == 'completed_item'){
+    $item= $_SESSION['items']['completed'][$id];
+    $completed_at=$items['completed_at'];
+    unset($_SESSION['items']['completed'][$id]);
+
+}else{
+    $item= $_SESSION['items']['todo'][$id];
+    $completed_at = null;
+    unset($_SESSION['items']['todo'][$id]);
+}
+
+$_SESSION['items']['deleted'][$id]=[
+    'title' => $item['title'],
+    'description' => $item['description'],
+    'created_at' => $item['created_at'],
+    'completed_at' => $completed_at,
+    'deleted_from' => $deleted_from,
+    'deleted_at'=>date("Y-m-d H:i:s")
+];
+
+$_SESSION['message']='The "'. $item['title'] . '" successfully deleted item';
+header("Location:../views/deleted.php");
+?>
---
+++ views/completed.php
@@ -0,0 +1,85 @@
+
+<?php
+
+ob_start();
+session_start();
+$completedItems=$_SESSION['items']['completed'];
+
+?>
+    
+    <div class="bg-gray-100 space-y-12 py-10 rounded-2xl">
+    <div>
+        <h3 class="text-3xl text-center font-source-code-pro">
+            Completed items
+        </h3>
+        <!-- ::if statement start here to show this message once;; -->
+        <?php if(key_exists('message',$_SESSION)){ ?>
+        <div class="bg-green-500 my-8 py-4 font-source-code-pro text-lg text-white text-center">
+            <!-- {$redirect_message} -->
+            <?php echo $_SESSION['message'];
+            unset($_SESSION['message']);?>
+        </div>
+        <!-- ::if statement end here to show this message once;; -->
+        <?php } ?>
+    </div>
+    <!-- completed item element -->
+    <div class="container mx-auto">
+        <?php $index=1; ?>
+        <!-- ::loop of items.completed should start here;; -->
+        <?php foreach($completedItems as $id => $completeditem) { ?>
+    
+        <div class="bg-white my-4 max-w-sm mx-auto rounded-2xl overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-105 cursor-pointer">
+            <div class="h-20 bg-green-500 flex items-center justify-start gap-3">
+                <h1 class="text-white ml-4 border-2 py-2 px-4 rounded-full">
+                    <!-- {$index}  -->
+                    <?php echo $index++; ?>
+                </h1>
+                <p class="mr-20 text-white text-lg">
+                    <!-- {$title} -->
+                    <?php echo $completeditem['title'];?>
+                </p>
+            </div>
+
+            <div class="flex items-center px-4 gap-3">
+                <form method="post" action="../actions/uncompleted.php" id="completed-item-<?php echo $id; ?>" class='my-0'>
+                    <input hidden name="item_id" value="<?php echo $id; ?>">
+                    <input type="checkbox" onclick="document.getElementById('completed-item-<?php echo $id; ?>').submit()"
+                           checked class='h-6 w-6 bg-white checked:scale-75 transition-all duration-200 peer'/>
+                </form>
+                <p class="py-6 text-lg tracking-wide gap-2 text-green-800">
+                    <!-- {$description} -->
+                    <?php echo $completeditem['description'];?>
+                </p>
+            </div>
+
+            <form action="../actions/assign-todo-item-as-deleted.php" method="post">
+                <input hidden name="item_id" value="<?php echo $id; ?>">
+                <input hidden name="delete_from" value="completed_item" />
+                <button type="submit"
+                        class="text-sm bg-red-500 text-white px-3 py-2 mx-4 rounded hover:bg-white hover:text-red-500 duration-500">
+                    Delete
+                </button>
+            </form>
+
+            <div class="flex justify-between px-5 my-4 text-sm text-gray-600">
+                <p>Completed at</p>
+                <p>
+                    <!-- {$created_at} -->
+                    <?php echo $completeditem['completed_at'];?>
+                </p>
+            </div>
+        </div>
+        <!-- ::loop of items.completed should end here;; -->
+        <?php } ?>
+    </div>
+</div>
+
+<?php
+
+$content=ob_get_contents();
+ob_get_clean();
+
+include 'template.php';
+?>
+
+
